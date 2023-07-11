87d1327a356f22c2571f0bfb15fa024287ee8160
jisooshin <kelly6226@naver.com>
2023-07-11 22:37:41 +0900

    [FEAT] bookmark 정보들 배치 완료

--- src/function/GetBookmarkAPI.jsx
+++ src/function/GetBookmarkAPI.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import './GetBookmarkAPI.css';
+
 function GetBookmarkAPI() {
   const [details, setDetails] = useState([]); // details 상태 변수 추가
 
@@ -22,15 +24,22 @@ function GetBookmarkAPI() {
   return (
     <div>
       {details.map((output, id) => (
-        <div key={id}>
-          <div>
-            <div>{output.shopName}</div>
-            <div>{output.description}</div>
-            <div>{output.distance}</div>
-            <div>{output.reviewCount}</div>
-            <div>{output.tag}</div>
-            <div>{output.open}</div>
+        <div key={id} id="shop">
+          <div className="shopInfo">
+            <div id="firstline">
+              <div className="shopName">{output.shopName}</div>
+              <div className="open">• 영업중</div>
+            </div>
+            <div className="description">{output.description}</div>
+            <div id="thirdline">
+              <div className="distance">{output.distance}M</div>
+              <div className="reviewCount">• 후기 {output.reviewCount}</div>
+            </div>
+            <div className="tag">{output.tag}</div>
+            <div className="open">{output.open}</div>
           </div>
+          <img id="cafeImg" src="/bookmarkImg/cafeImg.png" />
+          <img id="greyBar" src="/bookmarkImg/greybar.png" />
         </div>
       ))}
     </div>
