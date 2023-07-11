f9443e9851c646d3abc61d3150b92a2f96f88332
jisooshin <kelly6226@naver.com>
2023-07-11 13:17:29 +0900

    [FEAT] 북마크 선택시 추가 사이드바 오픈 제작 및 닫침버튼 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import GetBookmarkAPI from './function/GetBookmarkAPI';
 import Homepage from './pages/Homepage';
 import MakeMap from './function/MakeMap';
 
 function App() {
   return (
     <div className="App">
-      <GetBookmarkAPI />
       <MakeMap />
       <Homepage />
     </div>
--- src/pages/Bookmarkpage.jsx
+++ src/pages/Bookmarkpage.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
 import BookmarkMenubar from '../menu/BookmarkMenubar';
+import GetBookmarkAPI from '../function/GetBookmarkAPI';
+
+import './Bookmarkpage.css';
 
 function Bookmarkpage() {
   return (
     <div className="Home">
       <BookmarkMenubar />
+      <div className="bookmarkSidebar">
+        <div id="bar">
+          <div id="title">계용운님의 북마크</div>
+          <div id="middleBar">
+            <div id="bookmarkList">리스트</div>
+            <div id="articleList">아티클</div>
+          </div>
+        </div>
+        <div>
+          <GetBookmarkAPI />
+        </div>
+      </div>
+      <img id="closeButton" src="/buttonImg/closeButton.png" />
     </div>
   );
 }
