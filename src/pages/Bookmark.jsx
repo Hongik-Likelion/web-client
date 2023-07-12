d0e69331820c3a9ea3c53755070952d4107642da
soyeons <luvisallaround@naver.com>
2023-07-12 14:46:52 +0900

    [FEAT] Bookmark 통신 완료

--- src/pages/Bookmark.jsx
+++ src/pages/Bookmark.jsx
@@ -1,27 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './Bookmark.css';
 
-const zeroShop = [
-    {
-        shopName : '상점1',
-        description : '상점1 설명입니다 상점1',
-        distance : 100,
-        reviewCount : 14,
-        tag : ['태그1', '태그2'],
-        open : true
-    },
-    {
-        shopName : '상점2',
-        description : '상점2 설명입니다 상점2',
-        distance : 40,
-        reviewCount : 5,
-        tag : ['태그3', '태그4', '태그5'],
-        open : false
-    },
-];
-
 function Bookmark() {
+    const [bookmark, setBookmark] = useState();
+    
+    useEffect(()=>{
+        axios.get(`http://localhost:8000/bookmark`).then(response=>{
+            console.log(response.data);
+            setBookmark(response.data);
+        })
+
+    },[])
+
     return (
+    <>
         <div className="sidebar">
             <div className="title">
                 계용운님의 북마크
@@ -33,23 +26,25 @@ function Bookmark() {
                     </div>
                     
                     <div className="listDetail">
-                    {zeroShop.map((data, index) => (
-                        <div key={index}>
-                            <div style={{display: 'flex', alignItems: 'center'}}>
-                                <p style={{fontSize : '14px', fontWeight : 'bolder', marginRight : '15px'}}>{data.shopName}</p>
-                                <div className="circle1"></div>
-                                <p style={{fontSize : '1px', fontWeight : 'bolder', color : '#4AB2C9'}}>{data.open ? '영업중' : '영업 종료'}</p>
+                        {bookmark && bookmark.map((element, i) => (
+                            <div key={i}>
+                                <div>
+                                    {element.shopName}   
+                                </div>
+                                <div>
+                                    {element.description}   
+                                </div>
+                                <div>
+                                    {element.distance}   
+                                </div>
+                                <div>
+                                    {element.reviewCount}   
+                                </div>
+                                <div>
+                                    {element.tag}   
+                                </div>
                             </div>
-                            <p style={{fontSize : '12px', fontWeight : 'bold'}}>{data.description}</p>
-                            <div style={{display: 'flex', alignItems: 'center'}}>
-                                <p style={{fontSize : '10px', fontWeight : 'bolder', marginRight : '7px'}}>{data.distance}M</p>
-                                <div className="circle2"></div>
-                                <p style={{fontSize : '10px', color : '#6F6F6F', fontWeight : 'bolder'}}> 후기 {data.reviewCount}</p>
-                            </div>
-                            <p style={{fontSize : '8.52px', color : '#4AB2C9', marginBottom : '30px'}}>#{data.tag.join(' #')}</p>
-                            <hr className="separator" />
-                        </div>
-                    ))}
+                        ))}
                     </div>
                 </div>
                 {/* <div className="article">
@@ -62,6 +57,7 @@ function Bookmark() {
                 </div> */}
             </div>
         </div>
+    </>
     );
 }
 
